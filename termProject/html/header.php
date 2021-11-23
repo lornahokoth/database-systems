69be915277df0d29c22f4176b81b4825656518e0
lornahokoth <60718374+lornahokoth@users.noreply.github.com>
2021-11-23 01:13:21 -0500

    added logo

--- termProject/html/header.php
+++ termProject/html/header.php
@@ -1,17 +1,15 @@
 <header class="text-white bg-dark">
     <div class="header-row row align-items-center justify-content-between">
         <div class="col-4">
-            <a href="/" class="
+            <a href="/html/landing.php" class="
               d-flex
               align-items-center
               mb-3 mb-md-0
               me-md-auto
               text-white text-decoration-none
             ">
-                <svg class="bi me-2" width="40" height="32">
-                    <use xlink:href="#bootstrap:"></use>
-                </svg>
-                <span class="fs-4">Wavelength</span>
+                <img class="header-logo" src="/img/wavelength.png" alt="Wavelength Logo">
+                <span class="fs-4 span-logo"> Wavelength </span>
             </a>
         </div>
         <div class="col-4 text-end">
